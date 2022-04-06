343cc4673b4b8a961a7a2eebfc0ff072a120f1b0
Adriana Ruiz <adriannaruiz24@gmail.com>
2022-04-06 15:36:06 -0500

    cv

---
+++ src/components/Body.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+export const Body = () => {
+    return (
+        <div className='contenedor-body'>
+
+            <div className='cont-educa'>
+                <h2>Educación</h2>
+                <h4>Medica Veterinaria</h4>
+                <p>Universidad de La Salle</p>
+                <span>Caldas(Antioquia)-Colombia</span>
+                <hr />
+
+                <h4>Inlges</h4>
+                <p>Curso en Colombo Americano</p>
+                <span>Medellin-Colombia</span>
+                <hr />
+
+                <h4>UDEMY</h4>
+                <ul>
+                    <li>Programacion para principiantes - Primeros pasos (Fernando Herrera)</li>
+                    <li>Visual Studio Code: Mejora tu velocidad para codificar (Fernando Herrera)</li>
+                    <li>React: De cero a experto / hooks y MERN (Fernando Herrera)</li>
+                </ul>
+                <hr />
+
+                <h4>Proyectos</h4>
+                <ul>
+                    <li>https://adrianamariaruiz.github.io/grapeApp/</li>
+                    <li>https://adrianamariaruiz.github.io/app-dogs/</li>
+                    <li>https://adrianamariaruiz.github.io/smoothie-app/</li>
+                    <li>https://adrianamariaruiz.github.io/react-gifexpertapp/</li>
+                    <li>https://adrianamariaruiz.github.io/App_API_paises/</li>
+
+                </ul>
+
+            </div>
+
+        </div>
+    )
+}
---
+++ src/components/Sidebar.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+import foto from '../imagen/foto.jpg';
+
+export const Sidebar = () => {
+    return (
+        <div className='contenedor-sidebar'>
+            <div className='cont-img'>
+                <img
+                    src={foto}
+                    alt='Foto'
+                    className='mifoto'
+                />
+            </div>
+            <div className='cont-info'>
+                <h2>
+                    Adriana M. Ruiz Henao
+                </h2>
+                <hr />
+
+                <p>Sobre mi</p>
+                <span>
+                    Soy una persona comprometida, responsable y con sentido de pertenencia. Soy autodidacta y recursiva, por lo que busco, leo y me apoyo en amigos que tengan el conocimiento del tema.
+                    Manejo una comunicación asertiva por lo que suelo llevarme bien con los compañeros de trabajo.</span>
+                <hr />
+
+                <p> (+57)-300 391 6258  </p>
+                <span> celular </span>
+
+                <hr />
+
+                <div>
+                    <p>adriannaruiz24@hotmail.com</p>
+                    <span> correo persona </span>
+                </div>
+                <hr />
+
+                <p>Antioquia - Colombia</p>
+                <hr />
+
+                <div className='cont-conocimientos'>
+                    <p className='p-conoc'>
+                        Conocimientos
+                    </p>
+                    <ul className='lista'>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>Javascript</li>
+                        <li>React</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
